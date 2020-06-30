94f81a2c5c545d737c6b0feae33dc9ca40f7c0c2
Seba <sebastianpanetta@gmail.com>
2020-06-29 21:02:36 -0300

    Conexion via axios a API REST en Heroku

--- screens/Avisos.js
+++ screens/Avisos.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+
+import Card from '../components/Card';
 
 const axios = require('axios');
 
@@ -7,22 +9,63 @@ const Avisos = (props) => {
 
     const [mensajes, setMensajes] = useState([]);
 
-    axios.get('http://localhost:3000/api/mensaje')
+    var getMensajes = {
+        method: 'get',
+        url: 'https://administrapp-application.herokuapp.com/api/mensajes',
+        headers: {
+            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1ZWYzZDNlMzdkYjNmMDEyMWUzYjVkMzUiLCJpYXQiOjE1OTMwMzc4MTUsImV4cCI6MTU5NDI0NzQxNX0.RGiIy93Yj12KXLogjvLcW0-ROxXrmOS2iXXEvaGCEEM'
+        }
+    };
+
+    axios(getMensajes)
         .then(function (response) {
-            console.log(JSON.stringify(response));
-            setMensajes(JSON.stringify(response.data.mensajes));
+            console.log("Response: \n" + JSON.stringify(response));
+            const items = response.data.mensajes.map((item) => {
+                console.log("Item: " + JSON.stringify(item));
+                return (
+                    <Card style={styles.avisosContainer}>
+                        <Text style={styles.avisosText}>
+                            {item.descripcion}
+                        </Text>
+                    </Card>
+                );
+            })
+            console.log("Items: " + JSON.stringify(items));
+            if (mensajes.length == 0) {
+                setMensajes(items);
+            }
         })
         .catch(function (error) {
-            console.log("An error ocurred \n" + error);
+            console.log("An error ocurred \n");
+            console.log(error);
         })
 
     return (
-        <View>
-            <Text>
-                {mensajes}
-            </Text>
+        <View style={styles.screen}>
+            {mensajes}
         </View>
     )
 };
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    avisosContainer: {
+        flexDirection: 'row',
+        margin: 20,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        maxWidth: '80%',
+        width: 300,
+        height: 100,
+        maxHeight: '80%'
+    },
+    avisosText: {
+        fontSize: 18
+    }
+})
 
 export default Avisos;
