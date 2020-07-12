25ef99cda5e286db73de9e97ed9feaeccd8a8860
jfacuna15 <49376397+jfacuna15@users.noreply.github.com>
2020-07-12 19:42:08 -0300

    Renderizar dptos y floating button
    1) Implementar el getDepartamentos como está hecho en Avisos.js -> obtener el listado de deptos que estan en nuestro server
    2) Implementar Flatlist al listado de departamentos (como está en Avisos.js)
    3) Agregar floatingButton DepartamentosFloatingButton (copiando ExpensasFloatingButton) a la pantalla de departamentos.js

--- screens/Departamentos.js
+++ screens/Departamentos.js
@@ -1,9 +1,101 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Button, FlatList } from 'react-native';
 
-import Card from '../components/Card';
+import FloatingButton from '../components/FloatingButton';
+import ExpensasCard from '../components/ExpensasCard';
 import Colors from '../constants/colors';
 
+import Api from '../constants/api';
+import Session from '../constants/session';
+
+const axios = require('axios');
+
+const Departamentos = (props) => {
+
+    const [departamentos, setDepartamentos] = useState([]);
+    const [isAddMode, setIsAddMode] = useState(false);
+
+    var getDepartamentos = {
+        method: 'get',
+        url: `${Api.url}/departamentos`,
+        headers: {
+            'Authorization': `${Session.bearer}${Session.token}`
+        }
+    };
+    console.log("Request is: \n" + JSON.stringify(getDepartamentos));
+
+    axios(getDepartamentos)
+        .then(function (response) {
+            console.log("Response: \n" + JSON.stringify(response));
+            const items = response.data.departamentos.map((item) => {
+                console.log("Item: " + JSON.stringify(item));
+                return item;
+            })
+            if (departamentos.length == 0) {
+                setDepartamentos(items);
+            }
+            console.log("Departamentos: \n " + JSON.stringify(departamentos));
+        })
+        .catch(function (error) {
+            console.log("An error ocurred \n");
+            console.log(error);
+        })
+
+    // floating button solo para administradores
+    let floatingButton = <FloatingButton onPress={() => setIsAddMode(true)} />
+
+    return (
+    <View style={styles.expensasTitle}> 
+        <View style={styles.screen}>
+            <FlatList
+                keyExtractor={(item, index) => item._id}
+                data={departamentos}
+                renderItem={(itemData) =>
+                    <ExpensasCard
+                        key={itemData.item._id}
+                        id={itemData.item._id}
+                        title={itemData.item.titulo}
+                    />}
+            />
+        </View>
+        {floatingButton}
+    </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        backgroundColor: Colors.mainBackground,
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: 100
+    },
+    avisosContainer: {
+        flexDirection: 'row',
+        margin: 20,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        maxWidth: '80%',
+        width: 300,
+        height: 100,
+        maxHeight: '80%'
+    },
+    avisosText: {
+        fontSize: 18
+    },
+    expensasTitle: {
+        backgroundColor: Colors.mainBackground,
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop:100
+    },
+})
+
+export default Departamentos;
+
+/*
 const Departamentos = props => {
     return (
         <View style={styles.departamentosTitle}>
@@ -22,6 +114,7 @@ const Departamentos = props => {
         </View>
     );
 };
+
 
 const styles = StyleSheet.create({
     departamentosTitle: {
@@ -42,3 +135,4 @@ const styles = StyleSheet.create({
 });
 
 export default Departamentos;
+*/
